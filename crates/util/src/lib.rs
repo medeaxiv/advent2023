d86e5a8de7d3326a7dcb720a377dce51001192b7
Medea <105075687+medeaxiv@users.noreply.github.com>
2023-12-09 10:20:14 -0500

    use the num meta crate instead of num-traits

--- crates/util/src/lib.rs
+++ crates/util/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn greatest_common_divisor<T>(mut a: T, mut b: T) -> T
 where
-    T: num_traits::PrimInt,
+    T: num::Integer + Clone,
 {
     while b != T::zero() {
-        let t = b;
+        let t = b.clone();
         b = a % b;
         a = t;
     }
@@ -13,11 +13,12 @@ where
 
 pub fn least_common_multiple<T>(a: T, b: T) -> T
 where
-    T: num_traits::PrimInt,
+    T: num::Integer + Clone,
 {
     if a == T::zero() && b == T::zero() {
         return T::zero();
     }
 
-    (a * b) / greatest_common_divisor(a, b)
+    let gcd = greatest_common_divisor(a.clone(), b.clone());
+    (a * b) / gcd
 }
